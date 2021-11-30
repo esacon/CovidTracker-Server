fa7eb262ded48084c1f54dbdc979b36ed596c675
Esaco <esaco@uninorte.edu.co>
2021-11-29 23:18:55 -0500

    Diseño rutas

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const AdminController = require('../controllers/admin.controller');
+const AyudanteController = require('../controllers/ayudante.controller');
 
 // Administrador
 router.post('/admin', AdminController.create);
@@ -9,5 +10,21 @@ router.get('/admin', AdminController.getAll);
 router.get('/admin/:username', AdminController.getByUsername);
 router.put('/admin/:id', AdminController.update);
 router.delete('/admin/:usuario', AdminController.remove);
+
+// Ayudante.
+router.post('/ayudante', AyudanteController.create);
+router.get('/ayudante', AyudanteController.getAll);
+router.get('/ayudante', AyudanteController.getAll);
+router.get('/ayudante/:username', AyudanteController.getByUsername);
+router.put('/ayudante/:id', AyudanteController.update);
+router.delete('/ayudante/:usuario', AyudanteController.remove);
+
+// Cédula
+router.get('/paciente/:id_paciente/estados', AyudanteController.getAll);  // fecha_actualizacion, estado [{}]
+router.get('/paciente/:id_paciente/direcciones', AyudanteController.getAll); // objeto {residencia: [parseFloat(lat), parseFloat(longitud)], trabajo: [parseFloat(lat), parseFloat(longitud)]} valor unico
+router.get('/paciente', AyudanteController.getAll); // [{residencia: [parseFloat(lat), parseFloat(longitud)], color}]
+router.get('/casos')
+router.get('/casos/:fecha_inicio/:fecha_fin', AyudanteController.getAll) // {registrados: [{fecha, cantidad_casos}], }
+router.get('/muertos/:fecha_inicio/:fecha_fin', AyudanteController.getAll) // {}
 
 module.exports = router;
